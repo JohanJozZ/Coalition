c8565e2b8e66152bcb5520561d6b632ea8a87041
JohanJozZ <jjgomez1998@hotmail.com>
2024-07-15 16:45:45 -0400

    Added a dynamic chart and a menu to display blood pressure history

--- js/script.js
+++ js/script.js
@@ -1,7 +1,6 @@
 const patientsList = document.querySelector('.patients-list');
 const diagnosticsList = document.querySelector('.diagnostics-table');
 const labList = document.querySelector('.lab-list');
-
 const profilePictureFull = document.querySelector('.profile-picture-full');
 const profileName = document.querySelector('.profile-name');
 const dateBirth = document.querySelector('#date-of-birth');
@@ -10,7 +9,17 @@ const gender = document.querySelector('#gender');
 const contactInfo = document.querySelector('#contact-info');
 const emergencyContact = document.querySelector('#emergency-contact');
 const insuranceProvider = document.querySelector('#insurance-provider');
+const currentSys = document.getElementById('systolic');
+const currentDia = document.getElementById('diastolic');
+const avgSys = document.getElementById('average-sys');
+const avgDia = document.getElementById('average-dia');
+const avgDiaIcon = document.getElementById('average-dia-icon');
+const avgSysIcon = document.getElementById('average-sys-icon');
+const dropdownUL = document.querySelector('.dropdown-list');
+const dropdownList = document.querySelectorAll('.dropdown-list li');
+const dropdownCurrent = document.getElementById('dropdown-current');
 
+const chart = document.querySelector('#chart').getContext('2d');
 //Generate key to fetch data from API
 let API_user = 'coalition';
 let API_password = 'skills-test';
@@ -18,7 +27,8 @@ let auth = btoa(`${API_user}:${API_password}`);
 
 //Variable in order to populate content depending on patient selected
 let currentPatient = 3;
-
+let apiData = '';
+let myChart;
 fetch('https://fedskillstest.coalitiontechnologies.workers.dev', {
   headers: {
     authorization: `Basic ${auth}`,
@@ -27,10 +37,14 @@ fetch('https://fedskillstest.coalitiontechnologies.workers.dev', {
   .then((response) => response.json())
   .then((data) => {
     //use data to run functions that will populate the different website sections
+    apiData = data;
     fillPatientsList(data);
     fillDiagnosticsList(data, currentPatient);
     fillLabResults(data, currentPatient);
     fillProfile(data, currentPatient);
+    //Default months to show on pageload are 6
+    graphData(6);
+    fillGraph();
   })
   .catch((error) => error);
 
@@ -73,6 +87,97 @@ function fillPatientsList(data) {
   });
 }
 
+let bloodPressureDataSys = [];
+let bloodPressureDataDia = [];
+let chartLabels = [];
+
+function graphData(months) {
+  //Reset current data to 0
+  bloodPressureDataDia = [];
+  bloodPressureDataSys = [];
+  chartLabels = [];
+  const history = apiData[currentPatient].diagnosis_history;
+  for (let i = 0; i < months; i++) {
+    bloodPressureDataSys.unshift(history[i].blood_pressure.systolic.value);
+    bloodPressureDataDia.unshift(history[i].blood_pressure.diastolic.value);
+    chartLabels.unshift(`${history[i].month.slice(0, 3)}, ${history[i].year}`);
+  }
+
+  //Update chart datasets
+  if (myChart) {
+    myChart.data.labels = chartLabels;
+    myChart.data.datasets[0].data = bloodPressureDataSys;
+    myChart.data.datasets[1].data = bloodPressureDataDia;
+    myChart.update();
+  }
+}
+function fillGraph() {
+  //Get data, default period of time for the graph is 6 months.
+  const lastBloodPressure =
+    apiData[currentPatient].diagnosis_history[0].blood_pressure;
+
+  currentDia.textContent = lastBloodPressure.diastolic.value;
+  currentSys.textContent = lastBloodPressure.systolic.value;
+  avgDia.textContent = lastBloodPressure.diastolic.levels;
+  avgSys.textContent = lastBloodPressure.systolic.levels;
+
+  if (avgDia.textContent.includes('Lower')) {
+    avgDiaIcon.setAttribute('class', 'icon-arrowdown');
+  } else {
+    avgDiaIcon.setAttribute('class', 'icon-arrowup');
+  }
+  if (avgSys.textContent.includes('Lower')) {
+    avgSysIcon.setAttribute('class', 'icon-arrowdown');
+  } else {
+    avgSysIcon.setAttribute('class', 'icon-arrowup');
+  }
+
+  //Graph
+  myChart = new Chart(chart, {
+    type: 'line',
+    data: {
+      labels: chartLabels,
+      datasets: [
+        {
+          label: 'systolic',
+          data: bloodPressureDataSys,
+          tension: 0.4,
+          borderColor: '#E66FD2',
+          pointRadius: 8,
+          pointBackgroundColor: '#E66FD2',
+        },
+        {
+          label: 'diastolic',
+          data: bloodPressureDataDia,
+          tension: 0.4,
+          borderColor: '#8C6FE6',
+          pointRadius: 8,
+          pointBackgroundColor: '#8C6FE6',
+        },
+      ],
+    },
+    options: {
+      plugins: {
+        legend: {
+          display: false,
+        },
+      },
+      scales: {
+        x: {
+          ticks: {
+            color: '#072635',
+          },
+        },
+        y: {
+          ticks: {
+            color: '#072635',
+          },
+        },
+      },
+    },
+  });
+}
+
 function fillDiagnosticsList(data, current) {
   data[current].diagnostic_list.map((person) => {
     const name = document.createElement('div');
@@ -105,7 +210,6 @@ function fillLabResults(data, current) {
 
 function fillProfile(data, current) {
   const person = data[current];
-  console.log(person);
   profileName.innerHTML = person.name;
 
   dateBirth.innerHTML = person.date_of_birth;
@@ -125,3 +229,9 @@ function fillProfile(data, current) {
   // This is for the purpose of this SkillTest only, as we're not opening any other profile.
   // profilePictureFull.setAttribute('src', `${person.profile_picture}`);
 }
+
+dropdownList.forEach((li) => {
+  li.addEventListener('click', (e) => {
+    dropdownCurrent.textContent = e.target.innerHTML;
+  });
+});
